9e1493bf6d5398029a9b79d9431289cd83b1e6d9
Sander Sandvik Nessa <sandesn@stud.ntnu.no>
2025-03-31 18:46:19 +0200

    Initial backend boilerplate

---
+++ backend/src/main/java/no/ntnu/idatt2105/marketplace/MarketplaceApplication.java
@@ -0,0 +1,13 @@
+package no.ntnu.idatt2105.marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplaceApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/no/ntnu/idatt2105/marketplace/MarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ntnu.idatt2105.marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
