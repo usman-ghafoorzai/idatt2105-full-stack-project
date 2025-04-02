52f0a157bb93a3ff1ba589cbb9ad40aab9606df7
Usman <usmang@ntnu.no>
2025-04-03 00:26:59 +0200

    Add script for updating profile file path and updating image

--- frontend/src/components/UploadPicture.vue
+++ frontend/src/components/UploadPicture.vue
@@ -1,5 +1,11 @@
 <script setup>
+let profilePic = document.getElementById("profile-pic");
+let inputFile = document.getElementById("input-file");
 
+inputFile.onchange = function() {
+  profilePic.src = URL.createObjectURL(inputFile.files[0]);
+
+}
 </script>
 
 <template>
