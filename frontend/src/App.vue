f79d825537d883f2e87d8a577d577100ae852f95
Usman <usmang@ntnu.no>
2025-04-02 12:32:49 +0200

    Add LoginComponent

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Header from './components/Header.vue'
 import RegisterUser from './components/RegisterUser.vue'
+import LogInComponent from './components/LogInComponent.vue'
 </script>
 
 <template>
   <Header />
   <RegisterUser />
+  <LogInComponent/>
 </template>
 
 <style scoped>
