cebe58686d8bcb208c38d28914948a9fda7192ad
Garv Sood <garvs@stud.ntnu.no>
2025-04-02 17:17:31 +0200

    Implemented posting an item

--- backend/src/main/java/no/ntnu/idatt2105/marketplace/controller/ItemController.java
+++ backend/src/main/java/no/ntnu/idatt2105/marketplace/controller/ItemController.java
@@ -1,13 +1,22 @@
 package no.ntnu.idatt2105.marketplace.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
+import no.ntnu.idatt2105.marketplace.model.Item;
+import no.ntnu.idatt2105.marketplace.service.ItemService;
 
 @RestController
 @RequestMapping("/api/items")
 @CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class ItemController {
+  private final ItemService itemService;
   
+  @PostMapping
+  public ResponseEntity<Item> createItem(@RequestBody Item item) {
+    Item newItem = itemService.saveItem(item);
+    return ResponseEntity.ok(newItem);
+  }
 }
