ff398b327ca6db5bd94c249c536bbeb099962424
Usman <usmang@ntnu.no>
2025-04-03 00:21:00 +0200

    Create UploadPicture.vue, with generic avatar display and upload image button

---
+++ frontend/src/components/UploadPicture.vue
@@ -0,0 +1,59 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="profile-picture-container">
+    <div class="generic-avatar">
+      <img src="https://static.vecteezy.com/system/resources/previews/009/266/930/non_2x/user-icon-design-free-png.png"
+           alt="Generic avatar" class="avatar-image" id="profile-pic" />
+      <label for ="input-file">Upload image</label>
+      <input type="file" accept="image/jpeg, image/png, image/jpg" id="input-file">
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.profile-picture-container {
+  position: absolute;
+  width: 261px;
+  height: 256px;
+  left: 589px;
+  top: 208px;
+}
+
+.generic-avatar {
+  position: absolute;
+  width: 200px;
+  height: 200px;
+  left: 620px;
+  top: 208px;
+  background: #EADDFF;
+  border-radius: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+}
+
+.avatar-image {
+  max-width: 75%;
+  max-height: 75%;
+  object-fit: contain;
+}
+
+label {
+  display: block;
+  width: 200px;
+  background: cornflowerblue;
+  color: #fff;
+  padding: 12px;
+  margin: 10px auto;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+input {
+  display: none;}
+
+</style>
