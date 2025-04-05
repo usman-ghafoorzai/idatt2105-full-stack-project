732a3b910ee7528cc614c42669a64f63ccb77b1e
JoJoHTM <jojohansenhtm@gmail.com>
2025-04-05 17:18:38 +0200

    make grid for every item

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,11 +3,16 @@ import Header from './components/Header.vue'
 import RegisterUser from './components/RegisterUser.vue'
 import LogInComponent from './components/LogInComponent.vue'
 import ItemFrameComponent from './components/ItemFrameComponent.vue'
+import ItemListingComponent from './components/ItemListingComponent.vue'
+import FilterSidebar from './components/FilterSidebar.vue'
 </script>
 
 <template>
   <Header></Header>
-  <ItemFrameComponent></ItemFrameComponent>
+  <div id="filter">
+    <FilterSidebar></FilterSidebar>
+    <ItemListingComponent></ItemListingComponent>
+  </div>
 </template>
 
 <style>
@@ -15,4 +20,11 @@ body {
   margin: 0;
   font-family: 'Roboto', sans-serif;
 }
+#filter {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  height: 100vh;
+}
+
 </style>
