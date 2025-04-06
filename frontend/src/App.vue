c74489185f91df9c428a365907917969db45d714
JoJoHTM <jojohansenhtm@gmail.com>
2025-04-07 00:50:30 +0200

    create seller info component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,11 +6,12 @@ import ItemFrameComponent from './components/ItemFrameComponent.vue'
 import ItemListingComponent from './components/ItemListingComponent.vue'
 import FilterSidebar from './components/FilterSidebar.vue'
 import ItemDescription from './components/ItemDescription.vue'
+import SellerInfo from './components/SellerInfo.vue'
 </script>
 
 <template>
   <Header></Header>
-  <router-view/>
+  <router-view></router-view>
 </template>
 
 <style>
