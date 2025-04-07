7670be9a8769cb1ebdee44f0ae447aa38de11182
Usman <usmang@ntnu.no>
2025-04-07 15:23:24 +0200

    refactor: change name from Home.vue to HomeView.vue

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<script setup>
+import Header from '../components/Header.vue'
+import UserProfileComponent from '../components/UserProfileComponent.vue'
+</script>
+
+<template>
+  <div class="home-content">
+    <h1>Welcome to our marketplace</h1>
+
+    <!-- Temporary display of UserProfileComponent for development -->
+    <div class="profile-preview">
+      <UserProfileComponent />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.home-content {
+  padding: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.profile-preview {
+  margin-top: 30px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+</style>
