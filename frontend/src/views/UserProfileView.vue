14becddeb6fb898ec768dc68da528963a95a3d87
Usman <usmang@ntnu.no>
2025-04-07 21:23:51 +0200

    add: create UserProfileView.vue page

---
+++ frontend/src/views/UserProfileView.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import BookmarkedComponent from "@/components/BookmarkedComponent.vue";
+  import Header from '../components/Header.vue'
+  import ReservedComponent from "@/components/ReservedComponent.vue";
+  import UserProfileComponent from "@/components/UserProfileComponent.vue";
+  import
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
