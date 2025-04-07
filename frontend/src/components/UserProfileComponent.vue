2c18295d5ec58223a0049262bfcfe8d4113177ed
Usman <usmang@ntnu.no>
2025-04-07 15:24:51 +0200

    add: create template

--- frontend/src/components/UserProfileComponent.vue
+++ frontend/src/components/UserProfileComponent.vue
@@ -12,11 +12,23 @@ const user = {
 </script>
 
 <template>
-  <div>
-
+  <div class="user-profile-container">
+    <div class="account-box">
+      <div class="account-content">
+        <div class="profile-picture-wrapper">
+          <UploadPicture />
+        </div>
+        <div class="user-details">
+          <h3 class="user-name">{{ user.name }}</h3>
+          <h2 class="username">{{ user.username }}</h2>
+          <p class="user-email">{{ user.email }}</p>
+          <p class="user-phone">{{ user.phone }}</p>
+          <p class="user-location">{{ user.location }}</p>
+        </div>
+      </div>
+      <div class="bottom-box"></div>
+    </div>
   </div>
-
-
 </template>
 
 <style scoped>
