a837e839b8f174d1e8f0f2ac67305062b99dcb21
Usman <usmang@ntnu.no>
2025-04-07 15:19:55 +0200

    Create initial UserProfileComponent and add example user data

---
+++ frontend/src/components/UserProfileComponent.vue
@@ -0,0 +1,24 @@
+<script setup>
+import UploadPicture from './UploadPicture.vue';
+
+// Example user data - replace with actual data later
+const user = {
+  name: 'Usman Ghafoorzai',
+  username: 'usmangha',
+  email: 'usmangha@hotmail.com',
+  phone: '96803483',
+  location: 'Trondheim, Norway'
+};
+</script>
+
+<template>
+  <div>
+
+  </div>
+
+
+</template>
+
+<style scoped>
+
+</style>
