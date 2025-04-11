3450bf65eb35271354d42d8341da7bbaff66b9f3
JoJoHTM <jojohansenhtm@gmail.com>
2025-04-11 10:00:07 +0200

    remove comments

--- frontend/src/api/userAPI.js
+++ frontend/src/api/userAPI.js
@@ -35,21 +35,16 @@ export async function getUserImage(userId) {
     const response = await apiClient.get(`/users/${userId}/image`, {
       responseType: 'blob', // Ensure Axios treats the response as binary data
     });
-    
-    // Log the full response to inspect it
-    console.log('Full Response:', response);
 
-    // Check if the response contains a Blob (image data)
     if (response.data instanceof Blob) {
       console.log('Image Blob received:', response.data);
 
       // Convert the Blob to a Data URL using FileReader
       const reader = new FileReader();
       
-      // Return a Promise that resolves when the reader has finished converting
+
       return new Promise((resolve, reject) => {
         reader.onloadend = () => {
-          // Resolve with the data URL that can be used as the image source
           resolve(reader.result);
         };
         reader.onerror = reject;
@@ -82,7 +77,7 @@ export async function updateUser(userId, updatedFields) {
 export async function getSellerInformation(sellerId) {
   try {
     const response = await apiClient.get(`/users/${sellerId}`);
-    console.log(response.data);
+
     return response.data;
   } catch (error) {
     const errorMessage = error.response?.data?.message || "Failed to fetch seller information";
